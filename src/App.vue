58b6d7b523704de631b0a37b213b87c3925f518e
Zyber Loshka <75445321+Zyb3rWolfi@users.noreply.github.com>
2023-07-19 21:51:57 +0100

    Dashboard Layout and Map

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" class="px-24 bg-black">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- Navigation Bar -->
+    <nav-bar></nav-bar>
+
+    <!-- Invoices Goes Here -->
+    <div class="flex text-white mx-20 my-10 p-5 text-2xl rounded-t-lg" style="background: #1f2e42">
+        <h2>Invoice Overview</h2>
+    </div>
+
+    <!-- Actions Goes Here -->
+    <div class="flex text-white mx-20 my-10 p-5 text-2xl rounded-t-lg" style="background: #1f2e42">
+        <h2>Actions</h2>
+    </div>
+
+    <!-- Map Goes Here -->
+    <div class="flex text-white mx-20 my-10 p-5 text-2xl rounded-t-lg" style="background: #1f2e42">
+        <h2>Project Distribution Map</h2>
+    </div>
+    <map-comp></map-comp>
+
+    <!-- Bulletin Board Goes Here -->
+    <div class="flex text-white mx-20 my-10 p-5 text-2xl rounded-t-lg" style="background: #1f2e42">
+        <h2>Bulletin Board</h2>
+    </div>
+
+    <!-- Footer -->
+    <footer-comp></footer-comp>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navBar from "@/components/navBar.vue";
+import footerComp from "@/components/footerComp.vue";
+import mapComp from "@/components/mapComp.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      navBar,
+      footerComp,
+      mapComp,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+    font-family: 'filson-pro', sans-serif;
 }
 </style>
---
+++ src/components/footerComp.vue
@@ -0,0 +1,41 @@
+<template>
+    <footer class="bg-white rounded-lg shadow m-4">
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+                    <img src="https://loop.org.uk/wp-content/themes/loop/images/Loop_logo_blue_with_tag.svg" width="170" />
+                </a>
+                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Loop™</a>. All Rights Reserved.</span>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: 'footerComp',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
